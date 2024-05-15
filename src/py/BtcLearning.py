d5811e1ce54e42d12f4c566a27e5891da70812e7
donghaeK <rlaehdgo5598@naver.com>
2024-05-15 16:18:55 +0900

    commit

--- src/py/BtcLearning.py
+++ src/py/BtcLearning.py
@@ -1,161 +1,89 @@
-'''
-코인학습에 필요한 xml, json, csv파일 수집
-
-1. 석유 ㅇ
-2. 금 ㅇ
-3. 달러 ㅇ
-4. 나스닥 ㅇ
-5. 반감기 X (데이터존재X)
-   비트코인을 채굴할 수 있는 양을 반토막냄(가치를 높임)
-6. 수요와 공급(데이터)
-7. 채굴난이도 X (데이터존재X)
-
-각 Colum과 각 코인차트들의 시가,종가...를 매치해야할것같다.
-따라서 github에 각코인의 수만큼 csv가 필요할것같다.
-
-데이터 수집URL
-https://www.opec.org/opec_web/en/data_graphs/40.htm opec -석유수수출기구
-https://kr.investing.com/currencies/usd-krw-historical-data - 인베스팅닷컴
-'''
 
 
-import os
-from datetime import datetime, timedelta
+import requests
 import pandas as pd
-import numpy as np
-import pyupbit
-import tensorflow as tf
 import matplotlib.pyplot as plt
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Conv1D, Lambda
+from tensorflow.keras.losses import Huber
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+import os
+import datetime
+import numpy as np
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-#df = pd.read_csv('https://raw.githubusercontent.com/jjn3912/AI-coweb/main/src/csv/Coin.csv?token=GHSAT0AAAAAACQIYMABPLUQDZ2H5X3WELJIZQT43EQ' ,encoding='cp949')
-#df = pd.read_csv('C:/Coin.csv', encoding='cp949')
-#print(df.shape)
 
-
-# colum의 형태에 맞춰 split(), merge(), concat()
-# splited = oil_df['Date'].str.split(' ', expand=True)
-# print(splited)
-# splited.columns=['Date', 'oil Price']
-# print(splited)
-# splited.to_csv('oil_df.csv')
-# btc_df = pyupbit.get_ohlcv(ticker="KRW-BTC", interval="day1", to='20240402', count=2283)
-# Date_df = pd.read_csv('C:/Date.csv', encoding='cp949')
-# oil_df = pd.read_csv('C:/oil_df.csv', encoding='cp949')
-# usd_df = pd.read_csv('C:/Usd.csv', encoding='cp949')
-# Kospi_df = pd.read_csv('C:/Kospi.csv', encoding='cp949')
-# Gold_df = pd.read_csv('C:/Gold.csv', encoding='cp949')
-# splited = oil_df['Date'].str.split(' ', expand=True)
-# print(splited)
-# splited.columns=['Date', 'oil Price']
-# print(splited)
-# splited.to_csv('oil_df2.csv')
-# df = pd.merge(Date_df, oil_df, on='Date', how='outer')
-# df = pd.merge(df, usd_df, on='Date', how='outer')
-# df = pd.merge(df, Kospi_df, on='Date', how='outer')
-# df = pd.merge(df, Gold_df, on='Date', how='outer')
-# df = pd.concat([df, btc_df], axis=1)
-# df.to_csv('result.csv')
-
-#read_df = pd.read_csv('https://raw.githubusercontent.com/jjn3912/AI-coweb/main/src/csv/result.csv', encoding='cp949')
-
-# read_df = pd.read_csv('c:/result.csv', encoding='cp949')
-# df = read_df.dropna()
-# # print(df.columns)
-# # print(df.shape)
-# dates = pd.to_datetime(df["Date"])
-# X = list(df)[1:11]
-# df = df[X].astype(float)
-
-# def MinMaxScaler(data):
-#     numerator = data - np.min(data, 0)
-#     denominator = np.max(data, 0) - np.min(data, 0)
-#     return numerator / (denominator + 1e-7)
-#
-#
-# dfx = df[['oil Price', 'Usd', 'Kospi', 'gold', 'open', 'high', 'low', 'volume', 'value', 'close']]
-# dfx = MinMaxScaler(dfx)
-# dfy = dfx['close']
-# dfx = df[['oil Price', 'Usd', 'Kospi', 'gold', 'open', 'high', 'low', 'volume', 'value']]
-#
-# # 두 데이터를 리스트 형태로 저장
-# X = dfx.values.tolist()
-# y = dfy.values.tolist()
-#
-# window_size = 10
-#
-# data_X = []
-# data_y = []
-# for i in range(len(y) - window_size):
-#     _X = X[i : i + window_size] # 다음 날 종가(i+windows_size)는 포함되지 않음
-#     _y = y[i + window_size]     # 다음 날 종가
-#     data_X.append(_X)
-#     data_y.append(_y)
-#
-# train_size = int(len(data_y) * 0.7)
-# train_X = np.array(data_X[0 : train_size])
-# train_y = np.array(data_y[0 : train_size])
-#
-# test_size = len(data_y) - train_size
-# test_X = np.array(data_X[train_size : len(data_X)])
-# test_y = np.array(data_y[train_size : len(data_y)])
-#
-# print('훈련 데이터의 크기 :', train_X.shape, train_y.shape)
-# print('테스트 데이터의 크기 :', test_X.shape, test_y.shape)
-#
-# model = Sequential()
-# model.add(LSTM(units=20, activation='relu', return_sequences=True, input_shape=(10, 9)))
-# model.add(Dropout(0.1))
-# model.add(LSTM(units=20, activation='relu'))
-# model.add(Dropout(0.1))
-# model.add(Dense(units=1))
-# model.summary()
-#
-# model.compile(optimizer='adam', loss='mean_squared_error')
-# model.fit(train_X, train_y, epochs=50, batch_size=30)
-# pred_y = model.predict(test_X)  # test_X에 대한 예측값을 구합니다.
-#
-# plt.figure()
-# plt.plot(dates[train_size:], test_y, color='red', label='real SEC stock price')
-# plt.plot(dates[train_size:], pred_y, color='blue', label='predicted SEC stock price')
-# plt.title('SEC stock price prediction')
-# plt.xlabel('time')
-# plt.ylabel('stock price')
-# plt.legend()
-# plt.show()
-
-
-# df = pyupbit.get_ohlcv(ticker="KRW-BTC", interval = "minute1", to="20240510", count=1000000)
-# print(df)
-# df = pyupbit.get_ohlcv(ticker="KRW-BTC", interval = "minute1", to="20220614", count=1000000)
-# print(df)
-# df.to_csv(('Btcprice2.csv'))
-# df.to_pickle('Btcprice2.pickle')
-# df = pd.read_csv("Btcprice1.csv")
-# print(df)
-# df2 = pd.read_csv("Btcprice2.csv")
-# print(df2)
-# df.to_csv('btc min per price.csv')
-
-# import gzip
-#
-#
-# df2 = pd.read_pickle('btc min per price.pickle') #불러오기
-# print(df2)
-# with gzip.open('btc min per price.pickle.gz', 'wb') as f:
-#      f.write(df2.encode('utf-8'))
-
-import pandas as pd
-
-
-# 피클 파일에서 DataFrame 불러오기
-df = pd.read_csv("https://raw.githubusercontent.com/jjn3912/AI-coweb/main/src/py/Btcprice2.csv")
+# Github에서 분할된 DataFrame 불러오기 (1,000,000개씩)
+df1 = pd.read_csv("https://raw.githubusercontent.com/jjn3912/AI-coweb/main/src/py/Btcprice2.csv")
 df2 = pd.read_csv("https://raw.githubusercontent.com/jjn3912/AI-coweb/main/src/py/Btcprice1.csv")
-#print(df,df2)
+# 분할된 DataFrame 연결하기(약2,000,000개 / 2020 07 16 18:28 ~ 2024 05 10 08:59 (1분당 BTC가격 데이터)
+df = pd.concat([df1, df2])
 
-#df3 = pd.merge(df, df2, on='Date', how='outer')
-df3 = pd.concat([df,df2])
-print(df3)
+# 스케일을 적용할 column을 정의합니다.
+scaler = MinMaxScaler()
+scale_cols = ['open', 'high', 'low', 'close', 'volume', 'value']
+# 스케일 후 columns
+scaled = scaler.fit_transform(df[scale_cols])
+df = pd.DataFrame(scaled, columns=scale_cols)
+#print(df)
 
+x_train, x_test, y_train, y_test = train_test_split(df.drop('close', axis=1), df['close'], test_size=0.2, random_state=0, shuffle=False)
+x_train.shape, y_train.shape
+# print(type(x_test))
+# print(x_test)
+def windowed_dataset(x, y, window_size, batch_size, shuffle):
+    # X값 window dataset 구성
+    ds_x = tf.data.Dataset.from_tensor_slices(x)
+    ds_x = ds_x.window(window_size, shift=1, stride=1, drop_remainder=True)
+    ds_x = ds_x.flat_map(lambda x: x.batch(window_size))
+    # y값 추가
+    ds_y = tf.data.Dataset.from_tensor_slices(y[window_size:])
+    ds = tf.data.Dataset.zip((ds_x, ds_y))
+    if shuffle:
+        ds = ds.shuffle(1000)
+    return ds.batch(batch_size).prefetch(1)
+WINDOW_SIZE=5
+BATCH_SIZE=32
+train_data = windowed_dataset(x_train, y_train, WINDOW_SIZE, BATCH_SIZE, True)
+test_data = windowed_dataset(x_test, y_test, WINDOW_SIZE, BATCH_SIZE, False)
+
+model = Sequential([
+    # 1차원 feature map 생성
+    Conv1D(filters=32, kernel_size=5,
+           padding="causal",
+           activation="relu",
+           input_shape=[WINDOW_SIZE, 5]),
+    # LSTM
+    LSTM(16, activation='tanh'),
+    Dense(16, activation="relu"),
+    Dense(1),
+])
+
+# Sequence 학습에 비교적 좋은 퍼포먼스를 내는 Huber()를 사용합니다.
+loss = Huber()
+optimizer = Adam(0.0005)
+model.compile(loss=loss, optimizer=optimizer, metrics=['mse'])
+
+# earlystopping은 35번 epoch통안 val_loss 개선이 없다면 학습을 멈춥니다.
+earlystopping = EarlyStopping(monitor='val_loss', patience=35)
+
+history = model.fit(train_data,
+                    validation_data=(test_data),
+                    epochs=3,
+                    callbacks=[earlystopping])
+
+pred = model.predict(test_data)
+actual = np.asarray(y_test)[WINDOW_SIZE:]
+actual = np.reshape(actual, (len(actual), 1))
+
+plt.figure(figsize=(10,10))
+plt.plot(actual, label='actual')
+plt.plot(pred, label='prediction')
+plt.legend()
+plt.savefig('btc.png')
+plt.show()
--- src/py/진행상황.py
+++ src/py/진행상황.py
@@ -1,6 +1,26 @@
 '''
+
 History
+
 2024 04 04 비트코인 캔들차트 1분, 60분, 1일 생성
+2024 04 05
+코인학습에 필요한 xml, json, csv파일 수집
+1. 석유 ㅇ
+2. 금 ㅇ
+3. 달러 ㅇ
+4. 나스닥 ㅇ
+5. 반감기 X (데이터존재X)
+   비트코인을 채굴할 수 있는 양을 반토막냄(가치를 높임)
+6. 수요와 공급(데이터)
+7. 채굴난이도 X (데이터존재X)
+
+각 Colum과 각 코인차트들의 시가,종가...를 매치해야할것같다.
+따라서 github에 각코인의 수만큼 csv가 필요할것같다.
+
+데이터 수집URL
+https://www.opec.org/opec_web/en/data_graphs/40.htm opec -석유수수출기구
+https://kr.investing.com/currencies/usd-krw-historical-data - 인베스팅닷컴
+
 2024 04 07 AI 학습시킬 Api_Learning.py에 csv파일 정리 1~2번항목 (석유시세, 금가격) 까지 정리(github 개재)
 2024 04 08 학습시킬 Api_Learning.py에 csv파일 정리 3번항목
 
@@ -14,14 +34,29 @@ n달전데이터로 그다음달 에측-> 다음달 시가 예측 -> 코인인�
 2024 04 10 AI학습 고민중 csv데이터를 일별로 변환해야된다고 판단 후 변환중
 2024 04 11 프로젝트팀에게 csv를 일별로 변환한다고 상의 후 Coin.csv를 일별로 변환후 다시 merge진행
 2024 04 12 csv Kospi추가 후 일별 변환 완료 및 딥러닝 진행
-
-문제점
-코인이 하루주기가 의미가있나 의문이듬. 하지만 kospi, usd등 하루미만단위의 자료(초, 분)를 수집할 수 없음.
+2024 04 15 코인의 하루데이터도 의미없다고 판단, 원활한 데이터처리와 캔들차트삽입을 위해 분당데이터로만 처리
 일단 pyupbit에서 지원하는 KRW-BTC의 가격만을 분당데이터로 호출하여 학습시도했지만 대용량 데이터호출에 시간이 엄청걸리고 그마저도 호출이 안되는상황이 발생.
 
 조원들의 사정으로인해 2주동안 휴무
 
-2024 04 30 분당 데이터호출 되는지 확인, count가 150만개가 아닌 최소200는 되야 될것 같다 라고 판단 및 pickle객체사용으로 csv,json보다 더 적은 용량과 메모리로 동작할 수 있게 설정해보려함.
+2024 04 30 분당 데이터호출 되는지 확인, count가 150만개가 아닌 최소200만개는 되야 될것 같다 라고 판단 및 pickle사용으로 csv,json보다 더 적은 용량과 메모리로 동작할 수 있게 설정해보려함.
 참고 https://github.com/mverleg/array_storage_benchmark, https://wikidocs.net/163288
 github에 pickle등재 및 호출 되는지 테스트
+
+개인사정으로 10일정도 휴무
+
+2024 05 12~14 gitclone사용 및 github연동 후 200만개의 데이터를 lts사용으로 github에 등재하려 했지만 실패
+DataFrame을 100만개씩 분할 등록시켜 concat하는 방식으로 진행하여 학습할 DataFrame완성
+2024 05 15 완성시킨 DataFrame으로 AI학습(LSTM) 진행
+문제점
+1. 20%를 학습데이터로 나눠 학습을 진행하였는데 데이터가 너무 큰 관계로 1ephoc에 60s가 걸려버린다.(너무시간이 오래걸림)
+2. UserWarning: Your input ran out of data; interrupting training. Make sure that your dataset or generator can generate at least `steps_per_epoch * epochs` batches. You may need to use the `.repeat()` function when building your dataset.
+  self.gen.throw(value)
+입력한 데이터가 부족합니다. 훈련을 중단합니다. 데이터 세트 또는 생성기가 최소한 `steps_per_epoch * epochs` 배치를 생성할 수 있는지 확인하세요.
+데이터세트를 구축할 때 `.repeat()` 함수를 사용해야 할 수도 있습니다.
+모델 훈련중에 데이터부족 문제가 발생한다(원인을 모르겠다)
+3. Local rendezvous is aborting with status: OUT_OF_RANGE: End of sequence
+TensorFlow의 데이터셋 처리 과정에서 데이터가 끝에 도달했음에도 불구하고 추가 데이터를 요구할 때 발생할 수 있습니다.
+이는 데이터셋의 끝에 도달하여 더 이상 읽을 데이터가 없는 경우에 발생합니다.
+모델 훈련중에 데이터부족 문제가 발생한다(원인을 모르겠다22)
 '''
